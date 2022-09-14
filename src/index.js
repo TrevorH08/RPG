a404f069d60229dfa24556191d1ca2b60fe10454
TrevorH08 <trevorh8700@gmail.com>
2022-09-13 17:25:17 -0700

    WIP add battle menu, usability, and damage message
    Co-authored-by: Joseph Jackson <josephjacksonw@gmail.com>
    Co-authored by:  Samuel Majerus <ladolego@gmail.com>
    Co-authored-by: Skylan Lew <skylanslew@gmail.com>
    Co-authored-by: Spencer Dennis <sjdennis2113@gmail.com>

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ var Unit = new Phaser.Class({
   },
   attack: function(target) {
     target.takeDamage(this.damage);
+    this.scene.events.emit("Message", this.type + " attacks " + target.type + " for " + this.damage + " damage");
   },
   takeDamage: function(damage) {
     this.hp -= damage;
@@ -40,7 +41,7 @@ var PlayerCharacter = new Phaser.Class({
   function  PlayerCharacter(scene, x, y, texture, frame, type, hp, damage) {
     Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
 
-    // this.flipX = true;
+   // this.flipX = true;
 
     this.setScale(3);
   }
@@ -52,14 +53,15 @@ var MenuItem = new Phaser.Class({
   initialize:
 
   function MenuItem(x, y, text, scene) {
-    Phaser.GameObjects.Text.call(this, scene, x, y, text, { color: '#ffffff', align: 'left', fontSize: 30});
+    Phaser.GameObjects.Text.call(this, scene, x, y, text, { color: '#ffffff', align: 'left', fontSize: 30, lineSpacing: 20 });
+    
   },
   
   select: function() {
     this.setColor('#f8ff38');
   },
 
-  deslect: function() {
+  deselect: function() {
     this.setColor('#ffffff');
   }
 });
@@ -67,15 +69,131 @@ var MenuItem = new Phaser.Class({
 var Menu = new Phaser.Class({
   Extends: Phaser.GameObjects.Container,
 
-  initialze:
+  initialize:
 
   function Menu(x, y, scene, heroes) {
     Phaser.GameObjects.Container.call(this, scene, x, y);
+    this.menuItems = [];
+    this.menuItemIndex = 0;
+    this.heroes = heroes;
+    this.x = x;
+    this.y = y;
+  },
+  addMenuItem: function(unit) {
+    var menuItem = new MenuItem(0, this.menuItems.length * 20, unit, this.scene);
+    this.menuItems.push(menuItem);
+    this.add(menuItem);
+  },
+  moveSelectionUp: function() {
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex--;
+    if(this.menuItemIndex < 0)
+        this.menuItemIndex = this.menuItems.length - 1;
+    this.menuItems[this.menuItemIndex].select();
+  },
+  moveSelectionDown: function() {
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex++;
+    if(this.menuItemIndex >= this.menuItems.length)
+      this.menuItemIndex = 0;
+    this.menuItems[this.menuItemIndex].select();
+  },
+  select: function(index) {
+    if(!index)
+      index = 0;
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemsIndex = index;
+    this.menuItems[this.menuItemIndex].select();
+  },
+  deselect: function() {
+    this.menuItems[this.menuItemIndex].deselect();
+    this.menuItemIndex = 0;
+  },
+  confirm: function() {
     
+  },
+  clear: function() {
+    for(var i = 0; i < this.menuItems.length; i++) {
+      this.menuItems[i].destroy();
+    }
+    this.menuItems.length = 0;
+    this.menuItemIndex = 0;
+  },
+  remap: function(units) {
+    this.clear();
+    for(var i = 0; i < units.length; i++) {
+      var unit = units[i];
+      this.addMenuItem(unit.type);
+    }
   }
-})
+});
 
+var HeroesMenu = new Phaser.Class({
+  Extends: Menu,
 
+  initialize:
+  function HeroesMenu(x, y, scene) {
+    Menu.call(this, x, y, scene);
+  }
+});
+
+var ActionsMenu = new Phaser.Class({
+  Extends: Menu,
+
+  initialize:
+  function ActionsMenu(x, y, scene) {
+    Menu.call(this, x, y, scene);
+    this.addMenuItem('Attack');
+  },
+  confirm: function() {
+    this.scene.events.emit('SelectEnemies');
+  }
+});
+
+var EnemiesMenu = new Phaser.Class({
+  Extends: Menu,
+
+  initialize:
+  function EnemiesMenu(x, y, scene) {
+    Menu.call(this, x, y, scene);
+  },
+  confirm: function() {
+    this.scene.events.emit("Enemy", this.menuItemIndex);
+  }
+});
+
+var Message = new Phaser.Class({
+
+  Extends: Phaser.GameObjects.Container,
+
+  initialize:
+  function Message(scene, events) {
+    Phaser.GameObjects.Container.call(this, scene, 160, 30);
+    var graphics = this.scene.add.graphics();
+    this.add(graphics);
+    graphics.lineStyle(1, 0xffffff, 0.8);
+    graphics.fillStyle(0x031f4c, 0.3);
+    graphics.fillRoundedRect(150, 25, 180, 100);
+    graphics.strokeRoundedRect(150, 25, 180, 100);
+    graphics.fillRoundedRect(150, 25, 180, 100);
+    this.text = new Phaser.GameObjects.Text(scene, 240, 75, "", { color: '#ffffff', align: 'center', fontSize: 20, wordWrap: { width: 160, useAdvancedWrap: true }});
+    this.add(this.text);
+    this.text.setOrigin(0.5);
+    events.on("Message", this.showMessage, this);
+    this.visible = false;
+  },
+  showMessage: function(text) {
+    this.text.setText(text);
+    this.visible = true;
+    if(this.hideEvent)
+      this.hideEvent.remove(false);
+    this.hideEvent = this.scene.time.addEvent({ delay: 2500, callback: this.hideMessage, callbackScope: this});
+  },
+  hideMessage: function() {
+    this.hideEvent = null;
+    this.visible = false;
+  }
+});
 
 var BootScene = new Phaser.Class({
   Extends: Phaser.Scene,
@@ -105,6 +223,32 @@ var BattleScene = new Phaser.Class({
   {
       Phaser.Scene.call(this, { key: 'BattleScene' });
   },
+  nextTurn: function() {
+    this.index++;
+    // if there are no more units, we start again from the first one
+    if(this.index >= this.units.length) {
+      this.index = 0
+    }
+    if(this.units[this.index]) {
+      // if its player hero
+      if(this.units[this.index] instanceof PlayerCharacter) {
+        this.events.emit('PlayerSelect', this.index);
+      } else { // else if its enemy unit
+        // pick random hero
+        var r = Math.floor(Math.random() * this.heroes.length);
+        // call the enemy's attack function
+        this.units[this.index].attack(this.heroes[r]);
+        // add timer for the next turn, so it will have smoother gameplay
+        this.time.addEvent({ delay: 1500, callback: this.nextTurn, callbackScope: this });
+      }
+    }
+  },
+  receivePlayerSelection: function(action, target) {
+    if(action == 'attack') {
+      this.units[this.index].attack(this.enemies[target]);
+    }
+    this.time.addEvent({ delay: 1500, callback: this.nextTurn, callbackScope: this });
+  },
   create: function ()
   {
       // Run UI Scene at the same time
@@ -117,13 +261,14 @@ var BattleScene = new Phaser.Class({
       this.add.existing(greenslime);
       var redslime = new Enemy(this, 160, 200, 'slimerange', null, 'Red Slime', 50, 3);
       this.add.existing(redslime);
-
+      
       this.heroes = [monarch, noble];
       this.enemies = [greenslime, redslime];
       this.units = this.heroes.concat(this.enemies);
-
+      
       this.scene.launch('UIScene');
-  } 
+      this.index = -1;
+    } 
 });
 
 var UIScene = new Phaser.Class({
@@ -131,22 +276,89 @@ var UIScene = new Phaser.Class({
   initialize:
   function UIScene ()
   {
-      Phaser.Scene.call(this, { key: 'UIScene' });
+    Phaser.Scene.call(this, { key: 'UIScene' });
+  },
+  remapHeroes: function() {
+    var heroes = this.battleScene.heroes;
+    this.heroesMenu.remap(heroes);
+  },
+  remapEnemies: function() {
+    var enemies = this.battleScene.enemies;
+    this.enemiesMenu.remap(enemies);
+  },
+
+  onKeyInput: function(event){
+    if(this.currentMenu) {
+      if(event.code === "ArrowUp") {
+        this.currentMenu.moveSelectionUp();
+      } else if(event.code === "ArrowDown") {
+        this.currentMenu.moveSelectionDown();
+      } else if(event.code === "ArrowRight" || event.code === "Shift") {
+        
+      } else if(event.code === "Space" || event.code === "ArrowLeft") {
+        this.currentMenu.confirm();
+      }
+    }
+  },
+  onPlayerSelect: function(id) {
+    this.heroesMenu.select(id);
+    this.actionsMenu.select(0);
+    this.currentMenu = this.actionsMenu;
+  },
+  onSelectEnemies: function() {
+    this.currentMenu = this.enemiesMenu;
+    this.enemiesMenu.select(0);
+  },
+  onEnemy: function(index) {
+    this.heroesMenu.deselect();
+    this.actionsMenu.deselect();
+    this.enemiesMenu.deselect();
+    this.currentMenu = null;
+    this.battleScene.receivePlayerSelection('attack', index);
   },
   create: function ()
   {    
     this.graphics = this.add.graphics();
     this.graphics.lineStyle(5,0xffffff);
     this.graphics.fillStyle(0x031f4c, 1)
-    this.graphics.strokeRect(2, 277, 266, 200);
-    this.graphics.fillRect(2, 277, 266, 200);
-    this.graphics.strokeRect(268, 277, 266, 200);
-    this.graphics.fillRect(268, 277, 266, 200);
-    this.graphics.strokeRect(536, 277, 264, 200);
-    this.graphics.fillRect(536, 277, 262, 200);
+    this.graphics.strokeRoundedRect(2, 278, 260, 200);
+    this.graphics.fillRoundedRect(2, 278, 260, 200);
+    this.graphics.strokeRoundedRect(270, 278, 260, 200);
+    this.graphics.fillRoundedRect(270, 278, 260, 200);
+    this.graphics.strokeRoundedRect(538, 278, 260, 200);
+    this.graphics.fillRoundedRect(538, 278, 260, 200);
       
+    // basic container to hold all menus
+    this.menus = this.add.container();
+
+    this.heroesMenu = new HeroesMenu(548, 290, this);
+    this.actionsMenu = new ActionsMenu (288, 290, this);
+    this.enemiesMenu = new EnemiesMenu(12, 290, this);
+
+    // // // the currently selected menu
+    this.currentMenu = this.actionsMenu;
+
+    // // // add menus to the container
+    this.menus.add(this.heroesMenu);
+    this.menus.add(this.actionsMenu);
+    this.menus.add(this.enemiesMenu);
+
+    // //access battlescreen
+    this.battleScene = this.scene.get('BattleScene');
+    this.remapHeroes();
+    this.remapEnemies();
+
+    this.input.keyboard.on('keydown', this.onKeyInput, this);
+    this.battleScene.events.on("playerSelect", this.onPlayerSelect, this);
+
+    this.events.on("SelectEnemies", this.onSelectEnemies, this);
+    this.events.on("Enemy", this.onEnemy, this);
+    this.message = new Message(this, this.battleScene.events);
+    this.add.existing(this.message);
+    this.battleScene.nextTurn();
   }
 });
+
 
 var config = {
   type: Phaser.AUTO,
